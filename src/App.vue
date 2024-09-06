2da809683e67d47070da6d015013f4317fcc0f70
Chu Dang Nghia <nghia.cd@extremevn.com.vn>
2024-09-06 16:33:15 +0700

    displaying user profile

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+
+Amplify.configure(outputs);
 </script>
 
 <template>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
